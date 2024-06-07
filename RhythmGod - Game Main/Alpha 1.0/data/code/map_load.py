c171db4bb619d9b2ecc11952fe8af0b7a62c8307
Jiwootv <yooziwoo.coding0987@gmail.com>
2024-06-07 10:45:52 +0900

    폴더이름 변경

---
+++ RhythmGod - Game Main/Alpha 1.0/data/code/map_load.py
@@ -0,0 +1,26 @@
+import json
+
+class Chabo_map_load:
+	def __init__(self, mapname):
+		print("성공적으로 로드")
+		# self.file_name = open(mapname+"\\Map1.json", "r")
+		self.file_path = mapname + "\\Map1.json"
+		self.json_data = self.load()
+		print(self.json_data)
+	def load(self):
+		"""a = json.load(self.file_name)
+		print(a["chabo"])
+		for i in a["chabo"]:
+			print(i, a["chabo"][i])"""
+		with open(self.file_path, "r") as file:
+			a = json.load(file)
+			print(a["chabo"])
+			for i in a["chabo"]:
+				print(i, a["chabo"])
+		return a
+if __name__ == "__main__":
+	print("JSON LOAD")
+	C = Chabo_map_load("..\\map\\animals")
+
+
+# 해결함
